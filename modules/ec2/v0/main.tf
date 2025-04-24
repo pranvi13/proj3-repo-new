883e5a9006e75dee7a265e72cf376d0b0a9a5f3b
Jagruti Lone <jagrutilone11@gmail.com>
2025-04-24 13:02:00 +0530

    adding modules file of proj3 folder

---
+++ modules/ec2/v0/main.tf
@@ -0,0 +1,14 @@
+resource "aws_instance" "EC2" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = var.security_group_ids
+  disable_api_termination = var.enable_termination_protection
+  iam_instance_profile = var.instance_profile_name
+  user_data = var.user_data 
+  tags = var.tags
+  
+}
+
+
